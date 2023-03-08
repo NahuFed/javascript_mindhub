e8d47cf6c69c0e10726e8705d2180b18bdb59f8b
Nahuel Rodriguez <nahufed@gmail.com>
2023-03-08 15:58:20 -0300

    agregado funcion buscar en index

--- js/index.js
+++ js/index.js
@@ -10,20 +10,19 @@ contenedorCards.innerHTML = cardsGeneradas
 
 function crearCards(arrayDatos){
     let cards = ''
-    let n=0
+    
     for (const evento of arrayDatos){
-
         cards += `<aside class="col-12 col-md-6 col-lg-3 pb-5">
         <div class="card">
-          <img src= ${data.events[n].image} />
+          <img src= ${evento.image} />
           <div class="card-body d-flex flex-column">
-            <h5 class="card-title">${data.events[n].name}</h5>
-            <p class="card-text d-block flex-grow-1">${data.events[n].description}</p>                        
+            <h5 class="card-title">${evento.name}</h5>
+            <p class="card-text d-block flex-grow-1">${evento.description}</p>                        
             <div
               class="card-footer d-flex justify-content-between border-0 bg-white"
             >
-              <p>$ ${data.events[n].price}</p>
-              <a href="./pages/details.html?_id=${data.events[n]._id}" class="btn btn-outline-success"
+              <p>$ ${evento.price}</p>
+              <a href="./pages/details.html?_id=${evento._id}" class="btn btn-outline-success"
                 >Ver mas</a
               >
             </div>
@@ -31,7 +30,19 @@ function crearCards(arrayDatos){
         </div>
       </aside>`
         
-        n++
+        
     }
     return cards
 }
+
+
+
+
+
+let buscador = document.getElementById('buscador')
+
+buscador.addEventListener('change',()=>{
+  let eventosFiltrados = data.events.filter(evento => evento.name.toLowerCase().includes(buscador.value.toLowerCase()))  
+  let cardsFiltradas = crearCards(eventosFiltrados)
+  contenedorCards.innerHTML= cardsFiltradas
+})
