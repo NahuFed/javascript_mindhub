7ca50a9eb451d461fa7ca3b952ce6322f1d178fb
Nahuel Rodriguez <nahufed@gmail.com>
2023-03-18 13:05:51 -0300

    modificado codigo para funcionar con api

--- js/index.js
+++ js/index.js
@@ -1,76 +1,71 @@
 const contenedorCards = document.getElementById("contenedor-cards");
 const contenedorCheckboxes = document.getElementById("contenedor-checkboxes");
 
-let cardsGeneradas = crearCards(data.events);
-let checkBoxesGeneradas = crearCheckboxes(data.events);
 
-contenedorCards.innerHTML = cardsGeneradas;
-contenedorCheckboxes.innerHTML = checkBoxesGeneradas;
 
 let listArray = [];
 let buscador = document.getElementById("buscador");
 let formulario = document.querySelector("form");
 
-let eventosFiltrados = data.events;
 
-formulario.addEventListener("submit", (event) => {
-  event.preventDefault();
-  eventosFiltrados = data.events.filter(
-    (evento) =>
-      evento.name.toLowerCase().includes(buscador.value.toLowerCase()) ||
-      evento.description.toLowerCase().includes(buscador.value.toLowerCase())
-  );
-  actualizarCards(eventosFiltrados);
-});
 
-// let botonesCheckbox = document.querySelectorAll(".form-check-input");
+function traerDatos(){
+  fetch('https://mindhub-xj03.onrender.com/api/amazing')
+  .then(response => response.json())
+  .then(datosApi =>{
+    console.log(datosApi)    
+    let cardsGeneradas = crearCards(datosApi.events);
+    let checkBoxesGeneradas = crearCheckboxes(datosApi.events);
+    let eventosFiltrados = datosApi.events;
 
-// for (let checkbox of botonesCheckbox) {
-//   checkbox.addEventListener("click", function () {
-//     if (this.checked) {
-//       listArray.push(this.value);
-//     } else {
-//       listArray = listArray.filter((categoria) => categoria !== this.value);
-//     }
-//     eventosFiltrados = data.events.filter((evento) =>
-//       listArray.includes(evento.category)
-//     );
-//     eventosFiltrados = eventosFiltrados.filter(
-//       (evento) =>
-//         evento.name.toLowerCase().includes(buscador.value.toLowerCase()) ||
-//         evento.description.toLowerCase().includes(buscador.value.toLowerCase())
-//     );
-//     actualizarCards(eventosFiltrados);
-//   });
-// }
+    contenedorCards.innerHTML = cardsGeneradas;
+    contenedorCheckboxes.innerHTML = checkBoxesGeneradas;
 
-// Agregar el siguiente bloque de código para manejar el evento "submit" del formulario:
-formulario.addEventListener("submit", (event) => {
-  event.preventDefault();
+    formulario.addEventListener("submit", (event) => {
+      event.preventDefault();
+      eventosFiltrados = datosApi.events.filter(
+        (evento) =>
+          evento.name.toLowerCase().includes(buscador.value.toLowerCase()) ||
+          evento.description.toLowerCase().includes(buscador.value.toLowerCase())
+      );
+      actualizarCards(eventosFiltrados);
+    });
+    
+    // Agregar el siguiente bloque de código para manejar el evento "submit" del formulario:
+    formulario.addEventListener("submit", (event) => {
+      event.preventDefault();
+    
+      // Obtener los valores de los checkboxes marcados
+      let checkboxesMarcados = document.querySelectorAll(
+        'input[type="checkbox"]:checked'
+      );
+      let listArray = Array.from(checkboxesMarcados).map((checkbox) => {
+        return checkbox.value;
+      });
+    
+      // Filtrar los eventos según las categorías seleccionadas
+      eventosFiltrados = datosApi.events.filter((evento) =>
+        listArray.includes(evento.category)
+      );
+    
+      // Filtrar los eventos según el texto del buscador
+      eventosFiltrados = eventosFiltrados.filter(
+        (evento) =>
+          evento.name.toLowerCase().includes(buscador.value.toLowerCase()) ||
+          evento.description.toLowerCase().includes(buscador.value.toLowerCase())
+      );
+    
+      // Actualizar las cards con los eventos filtrados
+      actualizarCards(eventosFiltrados);
+    });
 
-  // Obtener los valores de los checkboxes marcados
-  let checkboxesMarcados = document.querySelectorAll(
-    'input[type="checkbox"]:checked'
-  );
-  let listArray = Array.from(checkboxesMarcados).map((checkbox) => {
-    return checkbox.value;
-  });
+  })
+  .catch(error => console.log(error.message))
+}
 
-  // Filtrar los eventos según las categorías seleccionadas
-  eventosFiltrados = data.events.filter((evento) =>
-    listArray.includes(evento.category)
-  );
+traerDatos()
 
-  // Filtrar los eventos según el texto del buscador
-  eventosFiltrados = eventosFiltrados.filter(
-    (evento) =>
-      evento.name.toLowerCase().includes(buscador.value.toLowerCase()) ||
-      evento.description.toLowerCase().includes(buscador.value.toLowerCase())
-  );
 
-  // Actualizar las cards con los eventos filtrados
-  actualizarCards(eventosFiltrados);
-});
 
 
 function actualizarCards(eventos) {
@@ -108,7 +103,7 @@ function crearCards(arrayDatos) {
 function crearCheckboxes(arrayDatos) {
   let checkboxes = "";
 
-  for (const evento of data.events) {
+  for (const evento of arrayDatos) {
     if (!checkboxes.includes(evento.category)) {
       checkboxes += `<div class="form-check col-12 col-md-6 col-lg-3">
       <input
